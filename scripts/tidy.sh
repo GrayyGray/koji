93077deac63e7418925138f984675e0c417cc274
GrayGray <gshillam@hotmail.com>
2026-09-16 15:18:18 -0700

    reversing logic to shorten the if  indents

--- scripts/tidy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-clang-format -i $(find src -name '*.cpp') $(find src -name '*.h') || exit 1
-clang-tidy --quiet --fix -p . $(find src -name '*.cpp') $(find src -name '*.h') || exit 1
--- src/frontend/windows/editor.cpp
+++ src/frontend/windows/editor.cpp
@@ -18,99 +18,115 @@ using namespace koji_library;
 
 void buttonUp(EditorContext &context, float button_size)
 {
-    if (ImGui::Button("⮝", ImVec2(button_size, button_size)))
+    if (!ImGui::Button("⮝", ImVec2(button_size, button_size)))
+        return;
+
+    vector<SongEntry>::iterator selected;
+
+    if (context.playlist_selected_song != SongEntry{})
     {
-        vector<SongEntry>::iterator selected;
-
-        if (context.playlist_selected_song != SongEntry{})
+        selected = find(context.playlist_container.begin(), context.playlist_container.end(), context.playlist_selected_song);
+        if (selected != context.playlist_container.end() && selected != context.playlist_container.begin())
         {
-            selected = find(context.playlist_container.begin(), context.playlist_container.end(), context.playlist_selected_song);
-            if (selected != context.playlist_container.end() && selected != context.playlist_container.begin())
-            {
-                context.playlist_container.erase(selected);
-                context.playlist_container.insert(selected - 1, context.playlist_selected_song);
-            }
+            context.playlist_container.erase(selected);
+            context.playlist_container.insert(selected - 1, context.playlist_selected_song);
         }
+    }
 
-        else if (context.basket_selected_song != SongEntry{})
+    else if (context.basket_selected_song != SongEntry{})
+    {
+        selected = find(context.basket_container.begin(), context.basket_container.end(), context.basket_selected_song);
+
+        if (selected != context.basket_container.end() && selected != context.basket_container.begin())
         {
-            selected = find(context.basket_container.begin(), context.basket_container.end(), context.basket_selected_song);
-
-            if (selected != context.basket_container.end() && selected != context.basket_container.begin())
-            {
-                context.basket_container.erase(selected);
-                context.basket_container.insert(selected - 1, context.basket_selected_song);
-            }
+            context.basket_container.erase(selected);
+            context.basket_container.insert(selected - 1, context.basket_selected_song);
         }
     }
 }
 
 void buttonDown(EditorContext &context, float button_size)
 {
-    if (ImGui::Button("⮟", ImVec2(button_size, button_size)))
+    if (!ImGui::Button("⮟", ImVec2(button_size, button_size)))
+        return;
+
+    vector<SongEntry>::iterator selected;
+
+    if (context.playlist_selected_song != SongEntry{})
     {
-        vector<SongEntry>::iterator selected;
-
-        if (context.playlist_selected_song != SongEntry{})
+        selected = find(context.playlist_container.begin(), context.playlist_container.end(), context.playlist_selected_song);
+        if (selected != context.playlist_container.end() && selected + 1 != context.playlist_container.end())
         {
-            selected = find(context.playlist_container.begin(), context.playlist_container.end(), context.playlist_selected_song);
-            if (selected != context.playlist_container.end() && selected != context.playlist_container.end() + 1)
-            {
-                context.playlist_container.erase(selected);
-                context.playlist_container.insert(selected + 1, context.playlist_selected_song);
-            }
+            context.playlist_container.erase(selected);
+            context.playlist_container.insert(selected + 1, context.playlist_selected_song);
         }
+    }
 
-        else if (context.basket_selected_song != SongEntry{})
+    else if (context.basket_selected_song != SongEntry{})
+    {
+        selected = find(context.basket_container.begin(), context.basket_container.end(), context.basket_selected_song);
+
+        if (selected != context.basket_container.end() && selected + 1!= context.basket_container.end())
         {
-            selected = find(context.basket_container.begin(), context.basket_container.end(), context.basket_selected_song);
-
-            if (selected != context.basket_container.end() && selected != context.basket_container.end() + 1)
-            {
-                context.basket_container.erase(selected);
-                context.basket_container.insert(selected + 1, context.basket_selected_song);
-            }
+            context.basket_container.erase(selected);
+            context.basket_container.insert(selected + 1, context.basket_selected_song);
         }
     }
 }
 
 void buttonLeft(EditorContext &context, float button_size)
 {
-    if (ImGui::Button("⮜", ImVec2(button_size, button_size)))
-    {
-        if (context.playlist_selected_song == SongEntry{} && context.basket_selected_song != SongEntry{})
-        {
-            vector<SongEntry>::iterator selected = find(context.basket_container.begin(), context.basket_container.end(), context.basket_selected_song);
+    if (!ImGui::Button("⮜", ImVec2(button_size, button_size)))
+        return;
 
-            if (selected == context.playlist_container.end())
-                return;
+    if (context.playlist_selected_song != SongEntry{} || context.basket_selected_song == SongEntry{})
+        return;
 
-            context.basket_container.erase(selected);
-            context.playlist_container.push_back(context.basket_selected_song);
-            context.basket_selected_song = {};
+    vector<SongEntry>::iterator selected = find(context.basket_container.begin(), context.basket_container.end(), context.basket_selected_song);
 
-            context.playlist_selected_song = context.playlist_container[context.playlist_container.size() - 1];
-        }
-    }
+    if (selected == context.playlist_container.end())
+        return;
+
+    context.basket_container.erase(selected);
+    context.playlist_container.push_back(context.basket_selected_song);
+    context.basket_selected_song = {};
+
+    context.playlist_selected_song = context.playlist_container[context.playlist_container.size() - 1];
 }
 
 void buttonRight(EditorContext &context, float button_size)
 {
-    if (ImGui::Button("⮞", ImVec2(button_size, button_size)))
-    {
-        if (context.playlist_selected_song != SongEntry{} && context.basket_selected_song == SongEntry{})
-        {
-            vector<SongEntry>::iterator selected = find(context.playlist_container.begin(), context.playlist_container.end(), context.playlist_selected_song);
+    if (!ImGui::Button("⮞", ImVec2(button_size, button_size)))
+        return;
 
-            if (selected == context.playlist_container.end())
-                return;
+    if (context.playlist_selected_song == SongEntry{} || context.basket_selected_song != SongEntry{})
+        return;
 
-            context.playlist_container.erase(selected);
-            context.basket_container.push_back(context.playlist_selected_song);
-            context.playlist_selected_song = {};
-            context.basket_selected_song   = context.basket_container[context.basket_container.size() - 1];
-        }
-    }
+    vector<SongEntry>::iterator selected = find(context.playlist_container.begin(), context.playlist_container.end(), context.playlist_selected_song);
+
+    if (selected == context.playlist_container.end())
+        return;
+
+    context.playlist_container.erase(selected);
+    context.basket_container.push_back(context.playlist_selected_song);
+    context.playlist_selected_song = {};
+
+    context.basket_selected_song = context.basket_container[context.basket_container.size() - 1];
+}
+
+void buttonSave(AppState &state, PlayerStatus &status, float button_size)
+{
+    if (!ImGui::Button("Save", ImVec2(button_size, button_size)))
+        return;
+}
+
+void buttonCancel(AppState &state, PlayerStatus &status, float button_size)
+{
+    if (!ImGui::Button("Cancel", ImVec2(button_size, button_size)))
+        return;
+
+    state.edit_window     = false;
+    status.editor_context = {};
 }
 
 void playlistEditorMenu(AppState &state, PlayerStatus &status)
@@ -146,28 +162,25 @@ void playlistEditorMenu(AppState &state, PlayerStatus &status)
     }
     ImGui::EndChild();
     ImGui::SameLine();
-    ImGui::BeginChild("buttons", ImVec2(button_size, table_size_y));
+    ImGui::BeginChild("buttons", ImVec2(button_size, table_size_y), ImGuiChildFlags_None, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
 
     buttonUp(status.editor_context, button_size);
     buttonDown(status.editor_context, button_size);
     buttonLeft(status.editor_context, button_size);
     buttonRight(status.editor_context, button_size);
 
-    // For saving and canceling buttons
-    const ImVec2 text_size      = ImGui::CalcTextSize("Cancel");
-    const float  scale          = std::min((button_size - margin * 2.0f) / text_size.x, (button_size - margin * 2.0f) / text_size.y);
-    const float  original_scale = ImGui::GetFont()->Scale;
+    const float original_font_scale = ImGui::GetFont()->Scale;
 
-    ImGui::SetWindowFontScale(scale);
+    // For saving and canceling buttons the font scaling is off and needs to be calculated
+    ImGui::SetWindowFontScale(std::min((button_size - margin * 2.0f) / 36.0f, (button_size - margin * 2.0f) / 83.0f));
 
-    if (ImGui::Button("Save", ImVec2(button_size, button_size)))
-    {
-    }
-    if (ImGui::Button("Cancel", ImVec2(button_size, button_size)))
-    {
-    }
+    // To set the buttons at the bottom of the editor
+    ImGui::SetCursorPosY(ImGui::GetCursorPosY() + ImGui::GetContentRegionAvail().y - (button_size * 2 + ImGui::GetStyle().FramePadding.y));
 
-    ImGui::SetWindowFontScale(original_scale);
+    buttonSave(state, status, button_size);
+    buttonCancel(state, status, button_size);
+
+    ImGui::SetWindowFontScale(original_font_scale);
 
     ImGui::EndChild();
     ImGui::SameLine();
@@ -209,8 +222,12 @@ void selectPlaylistMenu(AppState &state, PlayerStatus &status)
         ImGui::TableNextColumn();
 
         if (ImGui::Selectable(status.playlists[index].title.c_str(), false, ImGuiSelectableFlags_SpanAllColumns))
+        {
             status.editor_context.playlist_container = getPlaylistSongs(status.playlists[index]);
-
+            status.editor_context.playlist_container.reserve(status.editor_context.playlist_container.size() + status.editor_context.songs_to_append.size());
+            for (const SongEntry &song : status.editor_context.songs_to_append)
+                status.editor_context.playlist_container.push_back(song);
+        }
         ImGui::PopID();
     }
     ImGui::EndTable();
